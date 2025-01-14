106dc552cbd03fcba3fc80445793e7875b136eea
Crescent-Ao <crescent@wangaodeMacBook-Pro.local>
2025-01-14 14:49:21 +0800

    upload trt

--- quant/preprocess.py
+++ quant/preprocess.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import torch
 
 def process_detection(img: np.ndarray, input_size: list, stride: int = 32) -> np.ndarray:
     from utils.obb_util import letterbox
@@ -26,9 +27,11 @@ def process_segmentation(img: np.ndarray, input_size: list, stride: int = 32) ->
     """分割任务的预处理函数"""
     # 调整大小
     img = cv2.resize(img, (input_size[1], input_size[0]))
-    
-    # BGR to RGB and normalize
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, HWC to CHW
+    img = img.astype(np.float32)[:, :, ::-1]
+
     img = img.astype(np.float32) / 255.0
-    
+    img -= np.array([0.485, 0.456, 0.406])
+    img /= np.array([0.229, 0.224, 0.225])
+    img = img.transpose(2, 0, 1)  # HWC to CHW
+    img = torch.from_numpy(img).unsqueeze(0).cpu().numpy()
     return img 
